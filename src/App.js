ceb217980bf9642bbedda24ff2d047d3df975ced
Максим Ш <d.durnikin@neurone.ru>
2023-06-06 07:12:30 +0300

    fix css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
+import Registration from './Components/Registration';
+import Authorization from './Components/Authorization';
+import Footer from './Components/ComponentsHome/Footer';
+import SetcionTwo from './component/SetcionTwo';
+
+// import SetcionTwo from './component/SetcionTwo';
+// import Section
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+          {/* <Home /> */}
+          <Link to="/Register">Registration</Link>
+          <Link to="/Authorization">Authorization</Link>
+          <Routes>
+              <Route path='/Register' element={<Registration />}></Route>
+          </Routes>
+          <Routes>
+            <Route path='/Authorization' element={<Authorization />}></Route>
+          </Routes>
+          <Routes>
+            <Route path='/SectionTwo' element={<SetcionTwo />}></Route>
+          </Routes>
+          <Footer />
+          {/* <SetcionTwo /> */}
     </div>
   );
 }
